1f78895718ff9af70b760784676664c422364a4b
Amine Djeghri <amine.djeghri.pro@gmail.com>
2025-11-23 17:01:51 +0100

    :sparkles: add repo template files

--- tests/test_addon.py
+++ tests/test_addon.py
@@ -6,10 +6,10 @@ client = TestClient(app)
 
 
 def test_read_root():
-    """Test that root redirects to manifest."""
+    """Test that root redirects to configure page."""
     response = client.get("/", follow_redirects=False)
     assert response.status_code == 307
-    assert response.headers["location"] == "/manifest.json"
+    assert response.headers["location"] == "/configure"
 
 
 def test_manifest():
